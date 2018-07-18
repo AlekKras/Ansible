8c285fb6caceef14b6c6cd8122c2ee24d44c0f72
vagrant <vagrant@localhost.localdomain>
2018-07-18 14:03:39 +0000

    Added installation script for ansible

---
+++ ansible_install.sh
@@ -0,0 +1,1 @@
+(sudo yum install epel-release && sudo yum install ansible) || (sudo apt-get update; sudo apt-get install software-propertieis-common; sudo apt-add-repository ppa:ansible/ansible && sudo apt-get install ansible); ansible --version
